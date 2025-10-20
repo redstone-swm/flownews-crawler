51d822aef16824988fd841775bf759127b97a02a
sukkyun2 <goobghd@gmail.com>
2025-10-20 19:10:47 +0900

    feat: 2문장이내 요약으로 변경, 내용이 없는 기사의 경우 필터링

--- lambda_handler.py
+++ lambda_handler.py
@@ -17,14 +17,16 @@ def lambda_handler(event: dict, context: Any) -> dict:
         crawler = RSSFeedCrawler()
 
         # 크롤링
-        filtered_articles = list(filter(lambda x: not mongodb.is_duplicate(x["link"]), articles))
-        results = crawler.crawl(filtered_articles)
+        not_duplicated_articles = list(filter(lambda x: not mongodb.is_duplicate(x["link"]), articles))
+        results = crawler.crawl(not_duplicated_articles)
+
 
         # 전처리
         preprocessed_articles = preprocess(results)
+        filtered_articles = list(filter(lambda x: not is_badnews(x), preprocessed_articles))
 
         # 저장
-        inserted_ids = mongodb.save_articles(preprocessed_articles)
+        inserted_ids = mongodb.save_articles(filtered_articles)
 
         return {
             "statusCode": 200,
@@ -40,13 +42,19 @@ def lambda_handler(event: dict, context: Any) -> dict:
             "body": {"error": str(e)}
         }
 
+def is_badnews(article: dict) -> bool:
+    body = article.get("body", "")
+    return "새로운 연합뉴스" in body
+
 
 if __name__ == '__main__':
     load_dotenv()
 
+    is_badnews({"body": "이 기사는 새로운 연합뉴스에서 제공됩니다."})
+
     test_event = {
         "data": [{
-            "link": "https://www.yna.co.kr/view/AKR20250811149600005",
+            "link": "https://www.yna.co.kr/view/AKR20251020154600062",
             "title": "안규백, 1호 지휘서신…본립도생, 기본이 서야 길이 생긴다",
             "summary": "안규백 의원이 1호 지휘서신을 발표하며 본립도생의 중요성을 강조했다.",
             "date": "2023-10-01T12:00:00Z"
